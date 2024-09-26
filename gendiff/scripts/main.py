2f68205d0deee87de83b95d20dcaf71c3fd94940
Shaydullin Ilfat <sweden.fan@Yandex.ru>
2024-09-26 19:35:26 +0300

    step2 added -f -format flag

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -4,9 +4,10 @@ def main():
     parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
     parser.add_argument('first_file', help='Path to the first configuration file')
     parser.add_argument('second_file', help='Path to the second configuration file')
+    parser.add_argument('-f', '--format', help='set format of output (e.g., plain or json)', default='plain')
     args = parser.parse_args()
     
-    
+
     print(f"Comparing {args.first_file} and {args.second_file}")
 
 if __name__ == "__main__":
