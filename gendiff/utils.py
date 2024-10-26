53e8761998076055403a197b28a7e00f65d423a9
Shaydullin Ilfat <sweden.fan@Yandex.ru>
2024-10-26 14:02:49 +0300

    added yml open file function

--- gendiff/utils.py
+++ gendiff/utils.py
@@ -1,6 +1,19 @@
 import json
+import yaml
 
+def isJsonOrYaml(path_to_file):
+    pass
+    
 
 def open_file(path_to_file):
-    with open(path_to_file) as file_json:
-        return json.load(file_json)
+    if path_to_file.endswith('.json'):
+        with open(path_to_file) as file_json:
+            return json.load(file_json)
+
+    elif path_to_file.endswith('.yml') or path_to_file.endswith('.yaml'):
+        with open(path_to_file) as file_yaml:
+            return yaml.safe_load(file_yaml)
+    
+    else:
+        raise ValueError('Неверный формат файла')
+
